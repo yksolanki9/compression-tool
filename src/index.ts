f4beceab19f312b9d369798bb1a1c254dacb597c
Yash Solanki <yashsolanki1709@gmail.com>
2024-04-27 11:10:09 +0530

    Get count of each letter in file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
 #! /usr/bin/env node
 
-console.log("Hello world");
+import * as fs from "node:fs";
+
+const readFile = (filePath: string) => fs.readFileSync(filePath).toString();
+
+const countChars = (file: string) => {
+  const map: Record<string, number> = {};
+
+  for (let c of file) {
+    map[c] = (map[c] ?? 0) + 1;
+  }
+  return map;
+};
+
+const main = () => {
+  if (process.argv.length > 2) {
+    const filePath = process.argv[2];
+
+    const file = readFile(filePath);
+    const charDetails = countChars(file);
+
+    console.log(charDetails);
+  }
+};
+
+main();
